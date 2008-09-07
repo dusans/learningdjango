1c78eb92bbe292971bb5eb33a8d678e4e942fbdf
dusan.smitran <dusan.smitran@07f581b6-8f53-0410-8fb2-a3a96a0f6f5c>
2008-09-07 16:51:35 +0000

    Add lottery

--- trunk/polls/views.py
+++ trunk/polls/views.py
@@ -1,13 +1,45 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from prviDjango.polls.models import Poll, Choice
 from django.template import Context, loader
-from django.shortcuts import render_to_response, get_object_or_404
-from django.shortcuts import Http404
-from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404, Http404
 from django.core.urlresolvers import reverse
+from django import forms
+from django.forms import ModelForm
+from prviDjango.polls.models import Poll, Choice
 
+class PollForm(ModelForm):
+    class Meta:
+        model = Poll
 
+class ChoiceForm(ModelForm):
+    class Meta:
+        model = Choice
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField()
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
+def contactForm(req):
+    #poll = Poll.objects.get(pk=11)
+    choice = Choice.objects.get(pk=2)
+    form = ChoiceForm(instance=choice) #PollForm(instance=poll)
+    return render_to_response('polls/contact.html', {'form': form})
+
+def pollForm(req):
+    poll = Poll.objects.get(pk=2)
+    form = PollForm(instance=poll)
+    return render_to_response('polls/pollForm.html', {'form': form})
+
+def newPoll(req):
+    poll = Poll()
+    f = PollForm(req.POST, instance=poll)
+    if f.is_valid():
+        foo = f.save()
+        return HttpResponse('%s je stranjen' % foo)
+    return HttpResponse('Not valid')
 
 def vote(req, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
